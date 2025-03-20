38c72fa939feaa59756a76cf8c99a1a00c5a39da
rameshneel <ramesh@neelnewtworks.com>
2025-03-20 15:03:54 +0530

    refactor user routes for improved readability and organization

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,31 +1,25 @@
 import { Router } from "express";
 
-import { 
-    loginUser, 
-    logoutUser, 
-    registerUser, 
-    resetPasswordForForget,
-    forgetPasswordToken,
-    forgetPassword,
-   updateAccountDetails
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+  resetPasswordForForget,
+  forgetPasswordToken,
+  forgetPassword,
+  updateAccountDetails,
 } from "../controllers/user.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 // import { uploadSinglePhoto} from "../middlewares/multer.middleware.js";
 
-
-
-const router = Router()
-;
-router.route("/")
-    .post(
-        registerUser
-    );
-router.route("/login").post(loginUser)
+const router = Router();
+// router.route("/").post(registerUser);
+router.route("/login").post(loginUser);
 //secured routes
-router.route("/logout").post(verifyJWT,  logoutUser)
-router.route("/forget").post( forgetPassword)
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/forget").post(forgetPassword);
 router.route("/reset-password-token/:token").get(forgetPasswordToken);
-router.route('/reset-password/').patch(resetPasswordForForget);
-router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+router.route("/reset-password/").patch(resetPasswordForForget);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 
-export default router
+export default router;
